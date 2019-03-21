d67676fd0236f7e481864ebc855884add45d2331
SoconaL <SoconaL@DESKTOP-31662JG>
2019-03-21 16:22:03 +0800

    增加可空转换器

---
+++ Socona.ToolBox.Uwp/Converters/StringToNullableDoubleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Socona.ToolBox.Uwp.Converters
+{
+    public class StringToNullableDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        { return value; }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            double temp;
+            if (string.IsNullOrEmpty((string)value) || !double.TryParse((string)value, out temp)) return null;
+            else return temp;
+        }
+
+    }
+}
