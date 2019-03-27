25d98c414cca46576007e1a0c7d016698982799d
Socona <Socona@DESKTOP-VC6BPJT>
2019-03-28 01:59:04 +0800

    增加带宽计数器

---
+++ Socona.Toolbox/Networks/BandwidthCounter.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Threading;
+
+namespace Socona.ToolBox.Tools
+{
+    public class BandwidthCounter
+    {
+        private MiniCounter perSecond;
+        public long totalbytes;
+
+        /// <summary>
+        ///     Empty constructor, because thats constructive
+        /// </summary>
+        public BandwidthCounter()
+        {
+            perSecond = new MiniCounter { LastRead = DateTime.Now, Totalbytes = 0 };
+        }
+
+        /// <summary>
+        ///     Accesses the current transfer rate, returning the text
+        /// </summary>
+        /// <returns></returns>
+        public string GetPerSecond()
+        {
+            var s = perSecond + "/s";
+            perSecond = new MiniCounter { LastRead = DateTime.Now, Totalbytes = 0 };
+            return s;
+        }
+
+        /// <summary>
+        ///     Adds bytes to the total transfered
+        /// </summary>
+        /// <param name="count">Byte count</param>
+        public void AddBytes(long count)
+        {
+            long currentTotal;
+            long newtotal;
+            do
+            {
+                currentTotal = totalbytes;
+                newtotal = currentTotal + count;
+            } while (Interlocked.CompareExchange(ref totalbytes, newtotal, currentTotal) != currentTotal);
+            // overflow max
+            perSecond.AddBytes(count);
+        }
+
+        /// <summary>
+        ///     Prints out a relevant string for the bits transfered
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            long curtotal = totalbytes;
+
+            var pbyte = curtotal / (1024L * 1024 * 1024 * 1024 * 1024);
+            curtotal %= (1024L * 1024 * 1024 * 1024 * 1024);
+            var tbyte = curtotal / (1024L * 1024 * 1024 * 1024);
+            if (pbyte > 0)
+            {
+                var ret = pbyte + (double)tbyte / 1024;
+                var s = ret.ToString("#0.000");
+
+                return s + " PB";
+            }
+            curtotal %= (1024L * 1024 * 1024 * 1024);
+            var gbyte = curtotal / (1024 * 1024 * 1024);
+            if (tbyte > 0)
+            {
+                var ret = tbyte + (double)gbyte / 1024;
+
+                var s = ret.ToString("#0.000");
+
+                return s + " TB";
+            }
+
+            curtotal %= (1024L * 1024 * 1024);
+            var mbyte = curtotal / (1024 * 1024);
+            if (gbyte > 0)
+            {
+                var ret = gbyte + (double)mbyte / 1024;
+                var s = ret.ToString("#0.000");
+
+                return s + " GB";
+            }
+
+            curtotal %= (1024 * 1024);
+            var kbyte = curtotal / 1024;
+
+            if (mbyte > 0)
+            {
+                var ret = mbyte + (double)kbyte / 1024;
+
+                var s = ret.ToString("#0.000");
+
+                return s + " MB";
+            }
+            var nbyte = curtotal % 1024;
+
+            if (kbyte > 0)
+            {
+                var ret = kbyte + (double)nbyte / 1024;
+
+                var s = ret.ToString("#0.000");
+
+                return s + " KB";
+            }
+            else
+            {
+                var s = nbyte.ToString("#0.000");
+
+                return s + " B";
+            }
+        }
+
+        /// <summary>
+        ///     Class to manage an adapters current transfer rate
+        /// </summary>
+        private struct MiniCounter
+        {
+            private long totalbytes;
+
+            public long Totalbytes
+            {
+                get { return totalbytes; }
+                set { totalbytes = value; }
+            }
+
+            public DateTime LastRead { get; set; }
+
+            /// <summary>
+            ///     Adds bits(total misnomer because bits per second looks a lot better than bytes per second)
+            /// </summary>
+            /// <param name="count">The number of bits to add</param>
+            public void AddBytes(long count)
+            {
+                long currentTotal = 0;
+                long newtotal = 0;
+                do
+                {
+                    currentTotal = Totalbytes;
+                    newtotal = currentTotal + count;
+                } while (Interlocked.CompareExchange(ref totalbytes, newtotal, currentTotal) != currentTotal);
+                // Totalbytes += count;
+            }
+
+            /// <summary>
+            ///     Returns the bits per second since the last time this function was called
+            /// </summary>
+            /// <returns></returns>
+            public override string ToString()
+            {
+                var curtotal = Totalbytes;
+
+                var pbyte = curtotal / (1024L * 1024 * 1024 * 1024 * 1024);
+                curtotal %= (1024L * 1024 * 1024 * 1024 * 1024);
+                var tbyte = curtotal / (1024L * 1024 * 1024 * 1024);
+                if (pbyte > 0)
+                {
+                    var ret = pbyte + (double)tbyte / 1024;
+                    ret /= (DateTime.Now - LastRead).TotalSeconds;
+
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.000");
+
+                    return s + " PB";
+                }
+                curtotal %= (1024L * 1024 * 1024 * 1024);
+                var gbyte = curtotal / (1024 * 1024 * 1024);
+                if (tbyte > 0)
+                {
+                    var ret = tbyte + (double)gbyte / 1024;
+                    ret /= (DateTime.Now - LastRead).TotalSeconds;
+
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.00");
+
+                    return s + " TB";
+                }
+                curtotal %= (1024 * 1024 * 1024);
+                var mbyte = curtotal / (1024 * 1024);
+                if (gbyte > 0)
+                {
+                    var ret = gbyte + (double)mbyte / 1024;
+                    ret = ret / (DateTime.Now - LastRead).TotalSeconds;
+
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.00");
+
+                    return s + " GB";
+                }
+                curtotal %= (1024 * 1024);
+                var kbyte = curtotal / 1024;
+                if (mbyte > 0)
+                {
+                    var ret = mbyte + (double)kbyte / 1024;
+                    ret = ret / (DateTime.Now - LastRead).TotalSeconds;
+
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.00");
+
+                    return s + " MB";
+                }
+                var nbyte = curtotal % 1024;
+
+
+                if (kbyte > 0)
+                {
+                    var ret = kbyte + (double)nbyte / 1024;
+                    ret = ret / (DateTime.Now - LastRead).TotalSeconds;
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.00");
+
+                    return s + " KB";
+                }
+                else
+                {
+                    double ret = nbyte;
+                    ret = ret / (DateTime.Now - LastRead).TotalSeconds;
+                    LastRead = DateTime.Now;
+                    var s = ret.ToString("#0.00");
+
+                    return s + " B";
+                }
+            }
+        }
+    }
+}
